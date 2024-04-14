937735b30c615ceca5956ba16a089a82c6321cac
Phil <pbal@fordham.edu>
2024-04-14 09:50:50 -0700

    Fix up configs

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,4 @@
 export { default as DepositButton } from "./components/v1/DepositButton";
 
 // Contexts
-export { BoringVaultV1Provider } from "./contexts/v1/BoringVaultContextV1";
+export { BoringVaultV1Provider, useBoringVaultV1 } from "./contexts/v1/BoringVaultContextV1";
