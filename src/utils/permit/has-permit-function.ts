bed14a96660520a33a93a8b34efae67525bcffb6
lndgalante <lndgalante@gmail.com>
2025-04-09 18:36:28 -0300

    feat: Add permit support for DAI

--- src/utils/permit/has-permit-function.ts
+++ src/utils/permit/has-permit-function.ts
@@ -3,12 +3,13 @@ import { type Provider, keccak256, toUtf8Bytes } from 'ethers';
 export async function hasPermitFunction(provider: Provider, contractAddress: string): Promise<boolean> {
     const permitSignature = 'permit(address,address,uint256,uint256,uint8,bytes32,bytes32)';
     const permitSelector = keccak256(toUtf8Bytes(permitSignature)).slice(2, 10);
-    console.log('\n ~ hasPermitFunction ~ permitSelector:', permitSelector)
+
+    const permiteSignatureDai = 'permit(address,address,uint256,uint256,bool,uint8,bytes32,bytes32)';
+    const permiteSelectorDai = keccak256(toUtf8Bytes(permiteSignatureDai)).slice(2, 10);
 
     try {
         const contractByteCode = await provider.getCode(contractAddress);
-        console.log('\n ~ hasPermitFunction ~ contractByteCode:', contractByteCode)
-        return contractByteCode.includes(permitSelector);
+        return contractByteCode.includes(permitSelector) || contractByteCode.includes(permiteSelectorDai);
     } catch (error) {
         console.error(`Error checking permit function for address ${contractAddress}:`, error);
         return false;
