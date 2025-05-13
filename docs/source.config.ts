2d283e905d630d2d2a48a82eacd15c78c4a2b3de
lndgalante <lndgalante@gmail.com>
2025-05-13 16:21:15 -0300

    feat: Integrate fumadocs for our new documentation site

---
+++ docs/source.config.ts
@@ -0,0 +1,24 @@
+import {
+  defineConfig,
+  defineDocs,
+  frontmatterSchema,
+  metaSchema,
+} from 'fumadocs-mdx/config';
+import { remarkInstall } from 'fumadocs-docgen';
+
+// You can customise Zod schemas for frontmatter and `meta.json` here
+// see https://fumadocs.vercel.app/docs/mdx/collections#define-docs
+export const docs = defineDocs({
+  docs: {
+    schema: frontmatterSchema,
+  },
+  meta: {
+    schema: metaSchema,
+  },
+});
+
+export default defineConfig({
+  mdxOptions: {
+    remarkPlugins: [remarkInstall],
+  },
+});
