41ee71eaaab39d052614bd2c83935c90c2774267
Conor Gelvin <gelvin@outlook.com>
2025-05-27 17:44:39 -0700

    Working queue-withdraw.

--- src/solana/utils/constants.ts
+++ src/solana/utils/constants.ts
@@ -5,6 +5,9 @@ export const BASE_SEED_SHARE_TOKEN = 'share-token';
 export const BASE_SEED_ASSET_DATA = 'asset-data';
 export const CONFIG_SEED = 'config';
 
+// User withdraw state seed for Boring Queue
+export const BASE_SEED_USER_WITHDRAW_STATE = 'boring-queue-user-withdraw-state';
+
 // Default values
 export const DEFAULT_DECIMALS = 9; 
 
@@ -14,3 +17,5 @@ export const JITO_SOL_PRICE_FEED_ADDRESS = '4Z1SLH9g4ikNBV8uP2ZctEouqjYmVqB2Tz5S
 
 // Program ID
 export const BORING_VAULT_PROGRAM_ID = '5ZRnXG4GsUMLaN7w2DtJV1cgLgcXHmuHCmJ2MxoorWCE'; 
+export const BORING_QUEUE_PROGRAM_ID = '4yfE2VJQmxmcnUhrb8vdz7H8w313EZ3eJh5DbANBgtmd';
+export const TOKEN_2022_PROGRAM_ID = 'TokenzQdBNbLqP5VEhdkAS6EPFLC1PHnBqCXEpPxuEb'; 
