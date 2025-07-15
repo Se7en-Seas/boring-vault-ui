1896fa4f0d0883a5c83fc798578d755b63c1b496
Conor G Gelvin <gelvin@outlook.com>
2025-07-14 18:35:02 -0700

    solana exports are not from base package

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,3 @@ export { default as DepositButton } from "./components/v1/DepositButton";
 
 // Contexts
 export { BoringVaultV1Provider, useBoringVaultV1 } from "./contexts/v1/BoringVaultContextV1";
-
-// Solana SDK exports
-export * from "./solana/sdk";
-export * from "./solana/types";
-export * from "./solana/utils/constants";
