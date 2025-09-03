f852784e000aaa3c4c726b6b12c49d6273fc7665
Phil <pbal@fordham.edu>
2025-09-02 19:08:49 -0700

    demo cleanup

--- src/components/v1/DepositAndBridgeButton.tsx
+++ src/components/v1/DepositAndBridgeButton.tsx
@@ -286,7 +286,7 @@ const DepositAndBridgeButton: React.FC<DepositAndBridgeButtonProps> = ({
                   {...inputProps}
                 />
                 <FormHelperText>
-                  Maximum fee willing to pay for bridging. Note: You'll also need native tokens on the destination chain to pay for gas.
+                  Maximum fee willing to pay for bridging.
                 </FormHelperText>
               </FormControl>
 
